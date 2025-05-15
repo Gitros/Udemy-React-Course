2e8f3c0d2dfca1fb240c024c0c1ee162110b0c5b
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-15 20:58:57 +0200

    user input values change

--- Calculator/01-starting-project/src/App.jsx
+++ Calculator/01-starting-project/src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
 import CalculatorBox from "./components/CalculatorBox";
 import InvestmentResults from "./components/InvestmentResults";
 
 function App() {
-  const [value, setValue] = useState();
   return (
     <>
       <CalculatorBox />
