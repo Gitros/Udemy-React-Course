aed2f58c85f0d0f7df048e1f07ccc8c49fb04527
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-24 15:28:05 +0200

    creating input and new project

---
+++ ManagementApp2/01-starting-project/src/components/Input.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Input({ label, textArea, ...props }) {
+  return (
+    <p>
+      <label>{label}</label>
+      {textArea ? <textarea {...props} /> : <input {...props} />}
+    </p>
+  );
+}
